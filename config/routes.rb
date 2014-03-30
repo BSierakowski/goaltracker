25fa6e8c9ae5a2a68c3806ef5f20487dfe47e699
Brian Sierakowski <BrianSierakowski@gmail.com>
2014-03-30 11:57:12 -0400

    we have goals and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Goaltracker::Application.routes.draw do
+  resources :goals
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
