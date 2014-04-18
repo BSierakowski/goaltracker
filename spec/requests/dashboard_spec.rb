1d6f1df23753c5c82955ab2dc2a9e6db434434b8
Brian Sierakowski <BrianSierakowski@gmail.com>
2014-04-18 09:27:10 -0400

    space for new test

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -7,8 +7,13 @@ describe "Dashboard" do
     it "should have the content 'Your goals are'" do
       # user = create(:user)
       user = User.create(email: "brian@example.com")
+
       visit '/dashboard'
       expect(page).to have_content('Your goals are')
+    end
+
+    it "should automatically fill the user id when creating a goal" do
+      # do it
     end
 
   end
