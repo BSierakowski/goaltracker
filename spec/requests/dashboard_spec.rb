cfeb4c425053e25e1423c39f3d1e973bc75aad48
Henryvw <Henryvw@gmail.com>
2014-04-25 11:43:10 +0200

    added request spec test to check if ID pre-filled in on page

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,17 +3,24 @@ require 'spec_helper'
 describe "Dashboard" do
 
   describe "Main page" do
-    
+
     it "should have the content 'Your goals are'" do
       # user = create(:user)
-      user = User.create(email: "brian@example.com")
+      user = User.create(id: 1, email: "brian@example.com")
 
       visit '/dashboard'
       expect(page).to have_content('Your goals are')
     end
 
     it "should automatically fill the user id when creating a goal" do
-      # do it
+      # user = create(:id)
+        
+      user = User.create(id: 1)
+      visit '/dashboard'
+
+      expect(page).to have_text("l")
+           
+     
     end
 
   end
